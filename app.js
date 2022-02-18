edd6e6a4a13753468e715ad01b510d8e076b2910
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-02-18 12:36:22 +0100

    rechner

---
+++ app.js
@@ -0,0 +1,24 @@
+let screen = document.getElementById("screen");
+const allclear = () => {
+    screen.value = "";
+}
+
+const del = () => {
+    screen.value = screen.value.slice(0,-1);
+}
+
+const display = (num) => {
+    screen.value += num;
+}
+
+const equal = () => {
+    	
+			try{
+				screen.value = eval(screen.value);
+			}
+			catch{
+				
+				alert("Invalid Operation");
+
+			}
+}
